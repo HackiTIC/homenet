b5f107f341db324c73425200d809adb22b1d50e9
Erik Campobadal <ConsoleTVs@gmail.com>
2017-10-14 07:24:19 +0200

    Mas i mas ole ole :)

--- database/migrations/2017_10_13_221444_create_rooms_table.php
+++ database/migrations/2017_10_13_221444_create_rooms_table.php
@@ -15,6 +15,7 @@ class CreateRoomsTable extends Migration
     {
         Schema::create('rooms', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('house_id');
             $table->string('name');
             $table->float('temp', 5, 2);
             $table->boolean('light');
