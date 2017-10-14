bb2e5a25c9d5dbd3a40b5dc64281707bf9e8e08c
Erik Campobadal <ConsoleTVs@gmail.com>
2017-10-14 16:51:12 +0200

    Mas i mas ole ole :)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,11 @@
                 <div class="panel-heading">Charts</div>
                 <div class="panel-body">
                     @foreach (optional(auth()->user()->house)->rooms ? optional(auth()->user()->house)->rooms : [] as $room)
-                        {!! Charts::realtime(route('realtime.temp', $room), 1500, 'line', 'highcharts') ->render() !!}
+                        {!!
+                            Charts::realtime(route('realtime.temp', $room), 1500, 'line', 'highcharts')
+                            ->title($room->name)
+                            ->render()
+                        !!}
                     @endforeach
                 </div>
             </div>
