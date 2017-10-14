a4285af25294c295158c8a452a743affc9009ee7
Erik Campobadal <ConsoleTVs@gmail.com>
2017-10-14 06:59:52 +0200

    Mas i mas ole ole :)

--- routes/api.php
+++ routes/api.php
@@ -13,15 +13,19 @@ use Illuminate\Http\Request;
 |
 */
 Route::get('test', function () { return 'OK'; });
-Route::post('login', 'APIController@credentials')->name('login');
-Route::post('new_house', 'APIController@newHouse')->name('new_house');
 
-Route::middleware('auth:api')->as('app.')->prefix('app')->group(function () {
-    Route::post('info', 'APIController@user')->name('user');
-    Route::post('link_house', 'APIController@linkHouse')->name('link_house');
-    Route::post('rooms', 'APIController@appRooms')->name('rooms');
+Route::as('app.')->prefix('app')->group(function () {
+    Route::post('login', 'APIController@credentials')->name('login');
+    Route::middleware('auth:api')->group(function () {
+        Route::post('info', 'APIController@user')->name('user');
+        Route::post('link_house', 'APIController@linkHouse')->name('link_house');
+        Route::post('rooms', 'APIController@appRooms')->name('rooms');
+    });
 });
-Route::middleware('auth:house')->as('house.')->prefix('house')->group(function () {
-    Route::post('rooms', 'APIController@houseRooms')->name('rooms');
-    Route::post('new_room', 'APIController@newRoom')->name('new_room');
+Route::as('house.')->prefix('house')->group(function () {
+    Route::post('new_house', 'APIController@newHouse')->name('new_house');
+    Route::middleware('auth:house')->group(function () {
+        Route::post('rooms', 'APIController@houseRooms')->name('rooms');
+        Route::post('new_room', 'APIController@newRoom')->name('new_room');
+    });
 });
